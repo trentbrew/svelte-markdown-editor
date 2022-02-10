2bbe065f7c86672b030cf611e1dc2831232531c8
tbrew1023 <tbrew1023@gmail.com>
2022-02-09 17:18:54 -0800

    "weird padding"

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,102 @@
 <script>
-  import { marked } from 'marked'
+import { marked } from 'marked';
+import { onMount } from 'svelte';
 
-  let markdown = ""
+  let markdown = "";
+  let edit = false;
+
+  onMount(() => {
+	  console.log('mounted App.svelte');
+	  console.log(window.location.search);
+	  //console.log('db: ', firebase);
+	  if (window.location.search == '?superdupersecretpassword') {
+		  console.log('gotem');
+		edit = true;
+	  }
+  });
 </script>
 
 <main>
-	<h1>Markdown Editor</h1>
-	<textarea bind:value={markdown} placeholder="Enter markdown here"/>
 	<div class="preview">{@html marked(markdown)}</div>
+	{#if edit}
+		<div class="editor-container">
+			<div class="editor-head"><span>Editor</span><button>Publish</button></div>
+			<textarea bind:value={markdown} placeholder="Enter markdown here"/>
+		</div>
+	{/if}
 </main>
 
 <style>
-	textarea, .preview {
+	* {
 		box-sizing: border-box;
-		display: block;
+	}
+
+	main {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	.preview {
+		background: green !important;
+		padding: 36px;
+		overflow: auto;
 		width: 100%;
+	}
+
+	.editor-container {
+		height: 300px;
+		width: 100%;
+		background: blue;
 	}
 	
 	textarea {
+		background: #292929;
+		color: white;
+		font-size: 14px;
 		font-family: monospace, Roboto;
-		height: 25%;
 		border: none;
+		height: 100%;
+		width: 100%;
 		margin: 0;
-		padding: 1rem;
+		padding: 36px;
 	}
-	
-	.preview {
-		height: 75%;
-		padding: 2rem;
-		border-top: solid 2px #888;
+
+	textarea:focus, input:focus {
+		outline: none;
 	}
+
 	
-	h1 {
-		background: #e1e1e1;
+	.editor-head {
+		background: #212121;
+		color: white;
 		margin: 0px;
-		padding: 0.8rem;
 		font-size: 1.2rem;
+		display: flex;
+		height: 64px;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0px 24px;
+	}
+
+	button {
+		border-radius: 36px;
+		background: green;
+		color: white;
+		margin: 0px;
+		border: none;
+		padding: 6px 12px;
+		font-size: 18px;
+		font-weight: bold;
+		transition: 100ms;
+		border: 3px solid green;
+	}
+
+	button:hover {
+		cursor: pointer;
+		color: green;
+		border: 3px solid green;
+		background: #212121;
 	}
 	
 	:global(body) {
